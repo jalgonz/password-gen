60a2d3ec43f142736bb02f3e2acbb70a89742d73
jalgonz <81268765+jalgonz@users.noreply.github.com>
2021-05-15 22:39:48 -0400

    added css styling to header and generate button

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-
+// Arrays for character types
 var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+// Prompt to choose password length
 function generatePassword () {
     var chooseLength = (prompt("Choose your password length. (How many characters?)"));
 
     while(chooseLength <= 7 || chooseLength >= 129) {
         alert("Password length must range between 8-128 characters. Please choose a valid number. ");
         var chooseLength = (prompt("Choose your password length. (How many characters?)"));
-        //
     }
-
+    //Prompts to choose which character types to include
     var chooseLowerChar = confirm("Select OK to include lowercase characters.");
     var chooseUpperChar = confirm("Select OK to include uppercase characters.");
     var choosenNumber = confirm("Select OK to include numerical characters");
@@ -25,7 +25,7 @@ function generatePassword () {
             var choosenNumber = confirm("Select OK to include numerical characters");
             var chooseSpecialChar = confirm("Select OK to include special characters.");
         }
-
+    // This section concatinates the chosen character types into the password
     var passwordChar = []
     if (chooseLowerChar) {
         passwordChar = passwordChar.concat(lowerChar)
@@ -46,18 +46,30 @@ function generatePassword () {
     console.log(passwordChar)
 
     var generatedPassword = ""
-
+    //Selects randomly from the chosen character types until the amount of chosen characters is reached
     for (var i = 0; i < chooseLength; i++) {
         generatedPassword = generatedPassword + passwordChar[Math.floor(Math.random() * passwordChar.length)];
     }
     return generatedPassword;
 }
-
+//Writes the actual text for the generated password in the readonly text box
 function writePass() {
     var password = generatePassword();
     var passText = document.querySelector("#password");
 
     passText.value = password;
 }
-
+//Click to start password generation
 document.querySelector("#passGen").addEventListener("click", writePass);
+
+
+var copy = document.querySelector("#copy");
+copy.addEventListener("click", function () {
+    copyPass();
+});
+
+function copyPass() {
+    document.getElementById("password").querySelector();
+    document.execCommand("copy");
+    alert("Password copied to clipboard");
+}
